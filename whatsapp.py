b73575264ae79952150af68ba7e1ccadbb0391b9
machilika.l <lewis.machilika@nbs.mw>
2020-11-07 13:00:16 +0200

    tired

--- whatsapp.py
+++ whatsapp.py
@@ -1,7 +1,38 @@
 import pywhatkit as kit
+from tkinter import filedialog
+
+
 
 msg = input("Enter massege: ")
 num = input("Enter phone: ")
 
+import datetime
 
-kit.sendwhatmsg(num, msg, 8,52)
+def to_string_function(val):
+    return str(val)
+
+
+def sessionCalculator():
+    dt = datetime.datetime.today()
+    time=datetime.datetime.now().time().replace(microsecond=0)
+
+    return time
+
+print(to_string_function(sessionCalculator().hour) + " min " + to_string_function(sessionCalculator().minute+1))
+
+#kit.search("Python")
+#filedirectory = filedialog.askdirectory()
+#kit.image_to_ascii_art('D:\python\whatsapp\lewis.png',"luja.png")
+
+#search= input("what do you want to search? ")
+
+#text = kit.info(search,lines=4)
+
+#kit.text_to_handwriting(text,rgb=[0,0,0])
+kit.sendwhatmsg(num, msg, sessionCalculator().hour, sessionCalculator().minute+1)
+
+#kit.shutdown(time=100)
+
+#kit.shutdown(time=100)
+
+#kit.help()
